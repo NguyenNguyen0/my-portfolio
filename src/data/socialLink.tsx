0a64d72b8f0885406640476123866821ad345d7d
nguyennguyen0 <trungnguyenwork123@gmail.com>
2026-07-16 15:06:36 +0700

    refactor(socialLink): remove TopCV entry from social links

--- src/data/socialLink.tsx
+++ src/data/socialLink.tsx
@@ -13,10 +13,6 @@ export const socialLinks: SocialLink[] = [
 		url: 'https://www.linkedin.com/in/nguyennguyen0/',
 	},
 	{
-		label: 'TopCV',
-		url: 'https://www.topcv.vn/xem-cv/VA8HAANcCA5UDlcKVwwFBFQDCwMODgdSCABSAQ9a6b',
-	},
-	{
 		label: 'Zalo',
 		url: 'https://zalo.me/0394757329',
 	},
